a2cd934213c08c5ba50481085b8338d0122faf62
Alexander Mironov <alexander.mironov@zuhlke.com>
2025-11-26 15:44:44 +0000

    Add option to set default uncaught exception handler in ZuhlkeLogger

--- logging/src/androidMain/kotlin/com/zuhlke/logging/ZuhlkeLogger.android.kt
+++ logging/src/androidMain/kotlin/com/zuhlke/logging/ZuhlkeLogger.android.kt
@@ -2,11 +2,13 @@ package com.zuhlke.logging
 
 import android.app.Application
 import android.content.pm.ApplicationInfo
+import com.zuhlke.logging.data.Severity
 import com.zuhlke.logging.di.AndroidLoggingLibraryFactory
 import com.zuhlke.logging.integrations.kermit.KermitLogWriter
 import com.zuhlke.logging.integrations.room.RoomLogWriter
 import com.zuhlke.logging.interpolation.SafeInterpolation
 import com.zuhlke.logging.interpolation.UnsafeInterpolation
+import kotlin.system.exitProcess
 import kotlin.time.Clock
 
 public actual object ZuhlkeLogger {
@@ -16,12 +18,15 @@ public actual object ZuhlkeLogger {
      *
      * @param application The Android application instance.
      * @param useSafeInterpolation If true, uses safe interpolation to avoid logging sensitive data.
-     *                             Defaults to true in release builds and false in debug builds.
+     * Defaults to true in release builds and false in debug builds.
+     * @param setUncaughtExceptionHandler If true, sets a default uncaught exception handler to log uncaught exceptions.
+     * Defaults to true.
      */
     public fun initialize(
         application: Application,
         useSafeInterpolation: Boolean = application.applicationInfo.flags and
-            ApplicationInfo.FLAG_DEBUGGABLE == 0
+                ApplicationInfo.FLAG_DEBUGGABLE == 0,
+        setUncaughtExceptionHandler: Boolean = true
     ) {
         val interpolationConfiguration = if (useSafeInterpolation) {
             SafeInterpolation
@@ -43,5 +48,17 @@ public actual object ZuhlkeLogger {
             interpolationConfiguration,
             factory.getMetadata()
         )
+        if (setUncaughtExceptionHandler) {
+            Thread.setDefaultUncaughtExceptionHandler { thread, e ->
+                InnerLogger.shared.log(
+                    Severity.Error,
+                    "Uncaught exception",
+                    safeString("Uncaught exception in thread: ${public(thread.name)}"),
+                    e
+                )
+                Thread.sleep(1_000) // Give some time to log the message
+                exitProcess(1)
+            }
+        }
     }
 }
