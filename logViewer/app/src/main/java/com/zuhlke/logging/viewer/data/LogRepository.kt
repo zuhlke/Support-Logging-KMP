66c0c993bd92b8af71ef6c372a344ef848d68edf
Alexander Mironov <alexander.mironov@zuhlke.com>
2025-11-27 11:30:01 +0000

    Refactor log fetching and unique tags handling

--- logViewer/app/src/main/java/com/zuhlke/logging/viewer/data/LogRepository.kt
+++ logViewer/app/src/main/java/com/zuhlke/logging/viewer/data/LogRepository.kt
@@ -4,8 +4,11 @@ import android.util.Log
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
 
 class LogRepository @AssistedInject constructor(
     @Assisted val authority: String,
@@ -15,33 +18,36 @@ class LogRepository @AssistedInject constructor(
     private val allRuns = mutableListOf<AppRun>()
     private val allLogs = mutableListOf<LogEntry>()
 
-    private val _data = MutableStateFlow<List<AppRunWithLogs>>(emptyList())
-    val data: StateFlow<List<AppRunWithLogs>> = _data
+    fun getLogs(): Flow<List<AppRunWithLogs>> = flow {
+        var lastKnownAppRunId = 0
+        var lastKnownLogId = 0
 
-    suspend fun fetch(lastKnownAppRunId: Int, lastKnownLogId: Int): Pair<Int, Int> {
-        val newRuns = fetchAppRuns(authority, lastKnownAppRunId)
-        val newLogs = fetchLogs(authority, lastKnownLogId)
-        Log.d(
-            "LogRepository",
-            "Fetched ${newRuns.size} new runs and ${newLogs.size} new logs for authority $authority"
-        )
-        newRuns.forEach { allRuns.add(it) }
-        newLogs.forEach { allLogs.add(it) }
-        val logsByAppRunId = allLogs.groupBy { it.appRunId }
-        _data.value = allRuns.map { appRun ->
-            AppRunWithLogs(
-                appRun = appRun,
-                logEntries = logsByAppRunId[appRun.id] ?: emptyList()
+        while(true) {
+            val newRuns = fetchAppRuns(authority, lastKnownAppRunId)
+            val newLogs = fetchLogs(authority, lastKnownLogId)
+            Log.d(
+                "LogRepository",
+                "Fetched ${newRuns.size} new runs and ${newLogs.size} new logs for authority $authority"
             )
-        }
-        val lastKnownAppRunId = allRuns.lastOrNull()?.id ?: lastKnownAppRunId
-        val lastKnownLogId = allLogs.lastOrNull()?.id ?: lastKnownLogId
-        return lastKnownAppRunId to lastKnownLogId
-    }
+            if (newLogs.isNotEmpty() || newRuns.isNotEmpty()) {
+                allRuns.addAll(newRuns)
+                allLogs.addAll(newLogs)
+                lastKnownAppRunId = allRuns.lastOrNull()?.id ?: lastKnownAppRunId
+                lastKnownLogId = allLogs.lastOrNull()?.id ?: lastKnownLogId
 
-    fun getUniqueTagsSnapshot(): Set<String> = _data.value.flatMap {
-        it.logEntries
-    }.map { it.tag }.toSet()
+                val logsByAppRunId = allLogs.groupBy { it.appRunId }
+                val data = allRuns.map { appRun ->
+                    AppRunWithLogs(
+                        appRun = appRun,
+                        logEntries = logsByAppRunId[appRun.id] ?: emptyList()
+                    )
+                }
+                emit(data)
+            }
+
+            delay(1000)
+        }
+    }
 
     @AssistedFactory
     interface Factory {
